3a4375b1e54826ab7017be764e61e946c8facb61
Rudra Sharma <sharma_rudra@hotmail.com>
2025-01-16 18:03:17 +0530

    Setting up init data to controller

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,14 @@
 class Controller {
-    #initData = 'user=%7B%22id%22%3A6349140823%2C%22first_name%22%3A%22Rudra%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F4IQmXsErxMKTxy4bPce_ZxH3MppjWlnUv2ME030zzx96DNIyi-ZnSyPag7bplMcb.svg%22%7D&chat_instance=-6584738475927264663&chat_type=private&auth_date=1735579233&signature=xJubsgO_1N15GlOK4cz6L7s53eJYdo39mahLmor8ctGmoNRJdHrB9Uyx4XyVsz2PgESLSMUMvi8Rn6zzvEw6AA&hash=fb323c60e538e4682559f1e95762e1e1f7857edd7f68cdd224edc8034b1a7b01';
+    #initData = '';
     #baseApiUrl = null;
     #userInfoData = null;
     #leagueInfoData = null;
     constructor() {
         this.#baseApiUrl = (new Config()).getBaseApiUrl();
+        if (Telegram.WebApp.initData && Telegram.WebApp.initData !== "")
+            this.#initData = Telegram.WebApp.initData;
+        else
+            this.#initData = 'user=%7B%22id%22%3A6349140823%2C%22first_name%22%3A%22Rudra%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F4IQmXsErxMKTxy4bPce_ZxH3MppjWlnUv2ME030zzx96DNIyi-ZnSyPag7bplMcb.svg%22%7D&chat_instance=-6584738475927264663&chat_type=private&auth_date=1735579233&signature=xJubsgO_1N15GlOK4cz6L7s53eJYdo39mahLmor8ctGmoNRJdHrB9Uyx4XyVsz2PgESLSMUMvi8Rn6zzvEw6AA&hash=fb323c60e538e4682559f1e95762e1e1f7857edd7f68cdd224edc8034b1a7b01';
     }
 
     getLandingPageInfo(successCallback) {
