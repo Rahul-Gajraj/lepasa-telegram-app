d3e080293db0bb946e3757e42deb524ce710489c
GajrajRahul <rahulgajraj3497@gmail.com>
2025-02-12 17:34:36 +0530

    profit per hour issue fix

--- js/toast.js
+++ js/toast.js
@@ -2,7 +2,8 @@ class Toast {
     show(message) {
         $("#toast_div").stop().fadeIn(400)
         $("#toast_div").html(`<p class="toast_msg" id="toast_msg">${message}</p>`);
-        $("#toast_div").stop().fadeIn(400).delay(3000).fadeOut(500);
+        // $("#toast_div").stop().fadeIn(400).delay(3000).fadeOut(500);
+        $("#toast_div").stop().fadeIn(400).delay(700).fadeOut(500);
     }
 }
 const toast = new Toast();
